51d08e65e7c64d5bf430f3d31f247565d60d0423
Arhyel <arhyeltarfa2@gmail.com>
2023-01-05 17:47:30 +0100

    First commit

---
+++ .history/tailwind.config_20230105152850.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        back:"hsl(30, 38%, 92%)",
+        cyan:"hsl(158, 36%, 37%)"
+      },
+      fontFamily:{
+        heading:["Montserrat",'san-serif'],
+        price:["Fraunces",'sans-serif']
+      }
+
+    },
+  },
+  plugins: [],
+}
---
+++ .history/tailwind.config_20230105173655.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge:["./**/*"],
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        back:"hsl(30, 38%, 92%)",
+        cyan:"hsl(158, 36%, 37%)"
+      },
+      fontFamily:{
+        heading:["Montserrat",'san-serif'],
+        price:["Fraunces",'sans-serif']
+      }
+
+    },
+  },
+  plugins: [],
+}
